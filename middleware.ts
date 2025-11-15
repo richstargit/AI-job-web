f07eeb8d235e8516609ecf82ac65aa623c786bda
unknown <siwakchuhirun2004@gmail.com>
2025-11-15 21:37:47 +0700

    room chat

--- middleware.ts
+++ middleware.ts
@@ -5,34 +5,34 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const PUBLIC = ["/login", "/register", "/favicon.ico"];
 
 export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  const token = req.cookies.get("access_token")?.value || null;
+  // const { pathname } = req.nextUrl;
+  // const token = req.cookies.get("access_token")?.value || null;
 
-  // ให้ static ข้ามไปเลย
-  if (
-    pathname.startsWith("/_next") ||
-    pathname.startsWith("/static") ||
-    pathname.startsWith("/assets") ||
-    pathname.startsWith("/images") ||
-    pathname.startsWith("/api") ||  
-    pathname === "/favicon.ico"
-  ) {
-    return NextResponse.next();
-  }
+  // // ให้ static ข้ามไปเลย
+  // if (
+  //   pathname.startsWith("/_next") ||
+  //   pathname.startsWith("/static") ||
+  //   pathname.startsWith("/assets") ||
+  //   pathname.startsWith("/images") ||
+  //   pathname.startsWith("/api") ||  
+  //   pathname === "/favicon.ico"
+  // ) {
+  //   return NextResponse.next();
+  // }
 
-  if (PUBLIC.includes(pathname)) {
-    if (token) {
-      return NextResponse.redirect(new URL("/home", req.url));
-    }
-    return NextResponse.next();
-  }
+  // if (PUBLIC.includes(pathname)) {
+  //   if (token) {
+  //     return NextResponse.redirect(new URL("/home", req.url));
+  //   }
+  //   return NextResponse.next();
+  // }
 
-  // หน้า protected → ถ้าไม่มี token → redirect login
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // // หน้า protected → ถ้าไม่มี token → redirect login
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 
-  // มี token → ปล่อยผ่านทันที (ไม่ verify ที่ middleware)
+  // // มี token → ปล่อยผ่านทันที (ไม่ verify ที่ middleware)
   return NextResponse.next();
 }
 
